00518d5307560703ec1818a25daf1e2dc53f3f02
mostafa7611 <mostafanasiri7611@gmail.com>
2023-11-19 01:47:19 +0330

    Added image and video route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, All, Res, Req, Inject } from '@nestjs/common';
 
-@Controller('/')
+import { FileStore } from '@tus/file-store';
+import { Server } from '@tus/server';
+import { Request, Response } from 'express';
+
+@Controller('files')
 export class AppController {
-  constructor(private readonly myLovedService: AppService) {}
+  // we define it here as a property so
+  // it can be used in a bigger scope:
+  private tusInstanceforImage: Server;
+  private tusInstanceforVideo: Server;
 
-  @Get('whoToThank')
-  method() {
-    return this.myLovedService.getHello();
+  constructor() {
+    this.tusInstanceforImage = new Server({
+      respectForwardedHeaders: true,
+      path: '/files/image',
+      datastore: new FileStore({ directory: './files/image' }),
+    });
+    this.tusInstanceforVideo = new Server({
+      respectForwardedHeaders: true,
+      path: '/files/video',
+      datastore: new FileStore({ directory: './files/video' }),
+    });
+  }
+
+  @All(['image', 'image/*'])
+  uploadPhoto(@Res() res: Response, @Req() req: Request) {
+    this.tusInstanceforImage.handle(req, res);
+  }
+
+  @All(['video', 'video/*'])
+  uploadVideo(@Res() res: Response, @Req() req: Request) {
+    this.tusInstanceforVideo.handle(req, res);
   }
 }
